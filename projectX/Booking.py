34f6657521c36fb27c41d7b711ce8657420d7185
Felix Brilej <felix.brilej@googlemail.com>
2019-03-04 07:29:08 +0100

    Add basic tagging

--- projectX/Booking.py
+++ projectX/Booking.py
@@ -5,6 +5,7 @@ class Booking:
     iban = ""
     value = 0.0
     booking_type = ""
+    tag = ""
 
     def __init__(self, date, comment, from_to, iban, value, booking_type):
         self.date = date
@@ -15,4 +16,10 @@ class Booking:
         self.booking_type = booking_type
 
     def __str__(self):
-        return ','.join([self.date.strftime(format="%d.%m.%y"), self.comment, self.from_to, self.iban, str(self.value), self.booking_type])
+        return ','.join([self.date.strftime(format="%d.%m.%y"), 
+                         self.comment, 
+                         self.from_to, 
+                         self.iban, 
+                         str(self.value), 
+                         self.booking_type,
+                         self.tag])
